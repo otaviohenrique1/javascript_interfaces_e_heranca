807dd22d18b65e73de88acc2dbfd22f0f72bfbe9
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-10-31 18:45:58 -0300

    commit 6

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -1,5 +1,21 @@
 export class SistemaAutenticacao {
   static login(autenticavel, senha) {
+    if (SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+      return autenticavel.autenticar(senha);
+    }
+    return false;
+  }
+
+  static ehAutenticavel(autenticavel) {
+    /* Verifica se o "autenticar" existe no "autenticavel" e se ele é uma instancia de Function */
+    return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
+  }
+}
+
+/*
+export class SistemaAutenticacao {
+  static login(autenticavel, senha) {
     return autenticavel.autenticar(senha);
   }
 }
+*/
